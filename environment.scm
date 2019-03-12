23eccb83a5ef0b42c6c8aa310e4b531b5386ca39
John Soo <jsoo1@asu.edu>
2019-03-12 08:39:52 -0700

    Fix environment.scm

--- environment.scm
+++ environment.scm
@@ -7,19 +7,22 @@
    ghc-websockets))
  ((ghc-system) #:select (ghc-directory))
  ((ghc-optparse-generic) #:select (ghc-optparse-generic))
- ((gnu packages haskell) #:select (ghc-async))
+ ((gnu packages base) #:select (gnu-make))
+ ((gnu packages haskell) #:select (ghc ghc-async ghc-uuid))
  ((gnu packages haskell-web) #:select (ghc-wai))
  ((purescript) #:select (purescript purescript-spago)))
 
-'(ghc-aeson-1.4.2.0
-  ghc-async
-  ghc-conduit-1.3.0.3
-  ghc-directory
-  ghc-optparse-generic
-  ghc-servant-server
-  ghc-servant-websockets
-  ghc-uuid
-  ghc-wai
-  ghc-websockets
-  purescript
-  purescript-spago)
+`(,ghc
+  ,ghc-aeson-1.4.2.0
+  ,ghc-async
+  ,ghc-conduit-1.3.0.3
+  ,ghc-directory
+  ,ghc-optparse-generic
+  ,ghc-servant-server
+  ,ghc-servant-websockets
+  ,ghc-uuid
+  ,ghc-wai
+  ,ghc-websockets
+  ,gnu-make
+  ,purescript
+  ,purescript-spago)
